a368f0b04609ad1fec30824a70c8ccf317c45245
Lindomar Reitz <lindomar.reitz@gmail.com>
2019-06-15 14:46:45 +0100

    Add publish to pact broker

---
+++ publish.js
@@ -0,0 +1,10 @@
+let publisher = require("@pact-foundation/pact-node")
+let path = require("path")
+
+let opts = {
+    pactFilesOrDirs: [path.resolve(process.cwd(), "pacts")],
+    pactBroker: process.env.PACT_BROKER_URL,
+    consumerVersion: "1.0.0",
+}
+
+publisher.publishPacts(opts).then(() => done())
