9c3ccfe477d86a96435e0be1140f253e1922b8f9
Lindomar Reitz <lindomar.reitz@gmail.com>
2020-04-11 15:49:50 +0100

    Add token option

--- publish.js
+++ publish.js
@@ -4,6 +4,7 @@ let path = require("path")
 let opts = {
     pactFilesOrDirs: [path.resolve(process.cwd(), "pacts")],
     pactBroker: process.env.PACT_BROKER_URL,
+    pactBrokerToken: process.env.PACT_BROKER_TOKEN,
     consumerVersion: "1.0.0",
 }
 
